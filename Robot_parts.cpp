7870a447a3e40eea02e7c832e496a38b0de76645
nickryan07 <nickryan07@sbcglobal.net>
2017-03-28 07:10:09 -0500

    finished main method to create a robot part

--- Robot_parts.cpp
+++ Robot_parts.cpp
@@ -92,17 +92,51 @@ int main() {
     int part_num;
     double cost;
     cout << "---- Creating a robot part ----" << endl;
-    cout << "Enter a part name: ";
+    cout << "Enter a part name (one word): ";
     cin >> name;
-    cout << "Enter a part num: ";
+    cout << "Enter a part num (int): ";
     cin >> part_num;
-    cout << "Enter the part's cost: ";
+    cout << "Enter the part's cost (double): ";
     cin >> cost;
-    cout << "Enter a part description: ";
+    cout << "Enter a part description (one word): ";
     cin >> description;
     cout << "Specify type (Head, Arm, Torso, Battery, Motor): ";
     cin >> type;
-
+    if(type == "Torso" || type == "torso") {
+        int slots, arms;
+        cout << "Enter num battery slots: ";
+        cin >> slots;
+        cout << "Enter num of arms: ";
+        cin >> arms;
+        Torso *t = new Torso(slots, arms, name, part_num, description, cost);
+        cout << "Created new torso part named "<< name << endl;
+    } else if(type == "Head" || type == "head") {
+        double power;
+        cout << "Enter power: ";
+        cin >> power;
+        Head *h = new Head(power, name, part_num, description, cost);
+        cout << "Created new head part named "<< name << endl;
+    } else if(type == "Arm" || type == "arm") {
+        double arm_power;
+        cout << "Enter arm power: ";
+        cin >> arm_power;
+        Arm *a = new Arm(arm_power, name, part_num, description, cost);
+        cout << "Created new arm part named "<< name << endl;
+    } else if(type == "Motor" || type == "motor") {
+        double max_power;
+        cout << "Enter power: ";
+        cin >> max_power;
+        Motor *m = new Motor(max_power, name, part_num, description, cost);
+        cout << "Created new motor part named "<< name << endl;
+    } else if(type == "Battery" || type == "battery") {
+        double power_ava, max_energy;
+        cout << "Enter available power: ";
+        cin >> power_ava;
+        cout << "Enter max energy: ";
+        cin >> max_energy;
+        Battery *b = new Battery(power_ava, max_energy, name, part_num, description, cost);
+        cout << "Created new battery part named "<< name << endl;
+    }
     
     return 0;
 }
