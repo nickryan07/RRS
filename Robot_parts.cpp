1a4154b98c350f71f5926fef6bbd38da489cfd5b
nickryan07 <nickryan07@sbcglobal.net>
2017-03-28 05:45:19 -0500

    populated the Robot_parts class, created the Head class

--- Robot_parts.cpp
+++ Robot_parts.cpp
@@ -4,9 +4,18 @@
 
 class Robot_parts {
 
+protected:
+    string name;
+    int model_num;
+    string image_path;
+    string description
+    double cost;
 
+};
 
+class Head : public Robot_parts {
 
-
+private:
+    double power;
 };
 
