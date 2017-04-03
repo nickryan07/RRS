91a5cd1ee51e60cffadcd8ef32378766e3df8421
nickryan07 <nickryan07@sbcglobal.net>
2017-04-03 18:38:12 -0500

    update main method to contain a menu with a loop, added vectors to store created parts

--- Robot_parts.cpp
+++ Robot_parts.cpp
@@ -4,22 +4,7 @@
 
 using namespace std;
 
-class Robot_models {
-
-public:
-    double cost()
-    {
-    }
-    double max_speed()
-    {
-    }
-    double battery_life()
-    {
-    }
-
-};
-
-class Robot_parts : public Robot_models {
+class Robot_parts {
 
 public:
     Robot_parts(string name, int model_num, string description, double cost)
@@ -101,69 +86,101 @@ private:
     double max_power;
 };
 
+class Robot_models : public Robot_parts {
 
+public:
+    double cost()
+    {
+    }
+    double max_speed()
+    {
+    }
+    double battery_life()
+    {
+    }
+private:
+    string name;
+    int model_num;
+    Robot_parts torso;
+    Robot_parts motor;
+    Robot_parts arm;
+    Robot_parts battery;
+    Robot_parts head;
+
+};
 
 int main() {
     string type, name, description;
-    int part_num;
+    int part_num, command;
+    vector<Torso> torsos;
+    vector<Motor> motors;
+    vector<Arm> arms;
+    vector<Head> heads;
+    vector<Battery> battery;
+    vector<Robot_models> models;
     double cost;
-    cout << "---- Creating a robot part ----" << endl;
-    cout << "Enter a part name (one word): ";
-    cin >> name;
-    cout << "Enter a part num (int): ";
-    cin >> part_num;
-    cout << "Enter the part's cost (double): ";
-    cin >> cost;
-    cout << "Enter a part description (one word): ";
-    cin >> description;
-    cout << "Specify type (Head, Arm, Torso, Battery, Motor): ";
-    cin >> type;
-    if(type == "Torso" || type == "torso") {
-        int slots, arms;
-        cout << "Enter num battery slots: ";
-        cin >> slots;
-        cout << "Enter num of arms: ";
-        cin >> arms;
-        Torso *t = new Torso(slots, arms, name, part_num, description, cost);
-        cout << "Created new torso part named "<< name << endl;
-    } else if(type == "Head" || type == "head") {
-        double power;
-        cout << "Enter power: ";
-        cin >> power;
-        Head *h = new Head(power, name, part_num, description, cost);
-        cout << "Created new head part named "<< name << endl;
-    } else if(type == "Arm" || type == "arm") {
-        double arm_power;
-        cout << "Enter arm power: ";
-        cin >> arm_power;
-        Arm *a = new Arm(arm_power, name, part_num, description, cost);
-        cout << "Created new arm part named "<< name << endl;
-    } else if(type == "Motor" || type == "motor") {
-        double max_power;
-        cout << "Enter power: ";
-        cin >> max_power;
-        Motor *m = new Motor(max_power, name, part_num, description, cost);
-        cout << "Created new motor part named "<< name << endl;
-    } else if(type == "Battery" || type == "battery") {
-        double power_ava, max_energy;
-        cout << "Enter available power: ";
-        cin >> power_ava;
-        cout << "Enter max energy: ";
-        cin >> max_energy;
-        Battery *b = new Battery(power_ava, max_energy, name, part_num, description, cost);
-        cout << "Created new battery part named "<< name << endl;
+    while(true) {
+	cout << "------ Main Menu ------ " << endl;
+	cout << "(1) Create new parts." << endl;
+	cout << "(2) Define new models." << endl;
+        cout << "(3) Browse existing models." << endl;
+        cout << "(0) Quit." << endl;
+        cout << "Enter command: ";
+	cin >> command;
+        if (command != 1 && command != 2 && command != 3 && command != 0)
+            continue;
+        if (command == 1) {
+	    cout << "---- Creating a robot part ----" << endl;
+	    cout << "Enter a part name (one word): ";
+	    cin >> name;
+	    cout << "Enter a part num (int): ";
+	    cin >> part_num;
+	    cout << "Enter the part's cost (double): ";
+	    cin >> cost;
+	    cout << "Enter a part description (one word): ";
+	    cin >> description;
+	    cout << "Specify type (Head, Arm, Torso, Battery, Motor): ";
+	    cin >> type;
+	    if(type == "Torso" || type == "torso") {
+		int slots, arms;
+		cout << "Enter num battery slots: ";
+		cin >> slots;
+		cout << "Enter num of arms: ";
+		cin >> arms;
+		Torso *t = new Torso(slots, arms, name, part_num, description, cost);
+    torsos.push_back(*t);
+		cout << "Created new torso part named "<< name << endl;
+	    } else if(type == "Head" || type == "head") {
+		double power;
+		cout << "Enter power: ";
+		cin >> power;
+		Head *h = new Head(power, name, part_num, description, cost);
+		cout << "Created new head part named "<< name << endl;
+	    } else if(type == "Arm" || type == "arm") {
+		double arm_power;
+		cout << "Enter arm power: ";
+		cin >> arm_power;
+		Arm *a = new Arm(arm_power, name, part_num, description, cost);
+		cout << "Created new arm part named "<< name << endl;
+	    } else if(type == "Motor" || type == "motor") {
+		double max_power;
+		cout << "Enter power: ";
+		cin >> max_power;
+		Motor *m = new Motor(max_power, name, part_num, description, cost);
+		cout << "Created new motor part named "<< name << endl;
+	    } else if(type == "Battery" || type == "battery") {
+		double power_ava, max_energy;
+		cout << "Enter available power: ";
+		cin >> power_ava;
+		cout << "Enter max energy: ";
+		cin >> max_energy;
+		Battery *b = new Battery(power_ava, max_energy, name, part_num, description, cost);
+		cout << "Created new battery part named "<< name << endl;
+	    }
+        } else if (command == 0) {
+            break;
+        }
     }
-    
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
